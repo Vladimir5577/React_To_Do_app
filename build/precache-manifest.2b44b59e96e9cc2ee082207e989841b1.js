06f1f7dcb9207aa6d88e602b01031255fe873e50
Vladimir <vladimir160933@gmail.com>
2020-10-07 17:52:21 +0300

    added a build version

---
+++ build/precache-manifest.2b44b59e96e9cc2ee082207e989841b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786d85a11611d596a451a00cb724a471",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e3ac78ae2eb39f67616",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "428252bcabef3f3acd6e",
+    "url": "/static/js/2.aa9d478c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa9d478c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3ac78ae2eb39f67616",
+    "url": "/static/js/main.d56f3a07.chunk.js"
+  },
+  {
+    "revision": "35ed48a833016f336719",
+    "url": "/static/js/runtime-main.cb872f40.js"
+  }
+]);
